6cff81a40aea1b426fb6d2a7ddd2ac41b937a455
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-07-14 18:54:02 +0530

    vim options updated

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,29 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set history=1000")
+
+-- show the cursor position
+vim.cmd("set ruler")
+
+--show incomplete commands
+vim.cmd("set showcmd")
+--shows a menu while using tab completion
+vim.cmd("set wildmenu")
+
+-- height when you press z Enter
+vim.cmd("set scrolloff=5")
+
+vim.cmd("set hlsearch")
+vim.cmd("set incsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set number")
+--for line wrapping
+vim.cmd("set lbr")
+--auto indent
+vim.cmd("set ai")
+vim.cmd("set si")
+vim.cmd("set hidden")
 vim.g.mapleader = " "
