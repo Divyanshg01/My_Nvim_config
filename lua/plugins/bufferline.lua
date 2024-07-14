4abb194754490e73769cfc50e93e676224489f54
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-07-15 00:14:00 +0530

    bufferline working

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,1 +1,7 @@
-return {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
+return {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons' , 
+    config = function ()
+        require("bufferline").setup()
+        vim.keymap.set('n' , '<S-l>', ":bn<CR>" , {})
+        
+        vim.keymap.set('n' , '<S-h>' , ":bp<CR>" , {})
+    end }
