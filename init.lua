b0c685523f38a0421d2c848afc4f38ace496e7ce
Divyansh Gupta <divyanshgupta.dg123@gmail.com>
2024-08-01 23:27:09 +0530

    Update

--- init.lua
+++ init.lua
@@ -1,20 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldtext = ''
+vim.opt.foldlevelstart = 99
 require("vim-options")
 require("lazy").setup("plugins", {
-  change_detection = {
-    notify = false,
-  },
+    change_detection = {
+        notify = false,
+    },
 })
 require("lazy").setup("plugins")
 
